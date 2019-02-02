036ec6d3c8f1a066aff0aa1aaad68dcc9e5512c1
SkylakeLi <47272569+SkylakeLi@users.noreply.github.com>
2019-02-02 19:42:04 +0800

    Add files via upload

---
+++ 上海地铁猜车程序.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include<iostream>
+#include<cstdlib>
+#define DEVELOP
+using namespace std;
+void guess(int no)
+{
+	if (no == 92011 || no == 92022 || no == 92033 || no == 93442 || no == 93453 || no == 94102 || no == 94113 || no == 94121)
+		cout << 101 << endl;
+	else if (no == 93121 || no == 93262 || no == 93273 || no == 14652 || no == 14663 || no == 92102 || no == 93233 || no == 93311)
+		cout << 102 << endl;
+	else if (no == 92131 || no == 92142 || no == 92153 || no == 94022 || no == 94033 || no == 92162 || no == 92173 || no == 92181)
+		cout << 103 << endl;
+	else if (no == 92191 || no == 92202 || no == 92213 || no == 94202 || no == 94202 || no == 92222 || no == 92233 || no == 92241)
+		cout << 104 << endl;
+	else if (no == 93011 || no == 93022 || no == 93033 || no == 94162 || no == 94173 || no == 93042 || no == 93053 || no == 93061)
+		cout << 105 << endl;
+	else if (no == 92061 || no == 92053 || no == 92042 || no == 94053 || no == 94042 || no == 94093 || no == 94082 || no == 94071)
+		cout << 106 << endl;
+	else if (no == 93251 || no == 93142 || no == 93153 || no == 93462 || no == 93473 || no == 93282 || no == 93173 || no == 92121)
+		cout << 107 << endl;
+	else if (no == 93191 || no == 93202 || no == 93213 || no == 94142 || no == 94153 || no == 93222 || no == 92113 || no == 93361)
+		cout << 108 << endl;
+	else if (no == 93131 || no == 93322 || no == 93333 || no == 93402 || no == 93413 || no == 93162 || no == 93293 || no == 93301)
+		cout << 109 << endl;
+	else if (no == 93181 || no == 93353 || no == 93342 || no == 94233 || no == 94222 || no == 92093 || no == 92082 || no == 92071)
+		cout << 110 << endl;
+	else if (no == 93071 || no == 93082 || no == 93093 || no == 93382 || no == 93393 || no == 93102 || no == 93113 || no == 93241)
+		cout << 114 << endl;
+	else if (no == 98011 || no == 98022 || no == 98033 || no == 14672 || no == 14683 || no == 98042 || no == 98053 || no == 98061)
+		cout << 117 << endl;
+	else if (no == 99011 || no == 99022 || no == 99033 || no == 1802 || no == 1813 || no == 99042 || no == 99053 || no == 9906l)
+		cout << 118 << endl;
+	else if (no == 99071 || no == 99082 || no == 99093 || no == 1822 || no == 1833 || no == 99102 || no == 99113 || no == 9912l)
+		cout << 119 << endl;
+	else if (no == 99131 || no == 99142 || no == 99153 || no == 99502 || no == 99513 || no == 99162 || no == 99173 || no == 9918l)
+		cout << 120 << endl;
+	else if (no == 99191 || no == 99202 || no == 99213 || no == 99522 || no == 99533 || no == 99222 || no == 99233 || no == 9924l)
+		cout << 121 << endl;
+	else if (no == 99251 || no == 99262 || no == 99273 || no == 22 || no == 33 || no == 99282 || no == 99293 || no == 9930l)
+		cout << 122 << endl;
+	else if (no == 99311 || no == 99332 || no == 99333 || no == 42 || no == 53 || no == 99342 || no == 99353 || no == 9936l)
+		cout << 123 << endl;
+	else if (no == 99371 || no == 99382 || no == 99393 || no == 1742 || no == 1753 || no == 99402 || no == 99413 || no == 9942l)
+		cout << 124 << endl;
+	else if (no == 99431 || no == 99442 || no == 99453 || no == 1762 || no == 1773 || no == 99462 || no == 99473 || no == 9948l)
+		cout << 125 << endl;
+
+
+}
+void print_menu()
+{
+	system("cls");
+	cout << "欢迎来到上海地铁猜车程序V0.1" << "\n" << "由4编小鲶鱼工作室出品" << "\n" << "请输入车体号,输入0退出" << endl;
+    #ifdef DEVELOP
+	cout << "此版本为alpha版本,开发中,目前仅支持1号线老老八(101~110,114)与1号线老八(117~125)"<<endl;
+    #endif
+}
+int main()
+{
+	for (;;)
+	{
+		int ShanghaiMetro;
+		print_menu();
+		cin >> ShanghaiMetro;
+		if (ShanghaiMetro == 0)
+			break;
+		guess(ShanghaiMetro);
+		system("pause");
+	}
+	return 0;
+}
